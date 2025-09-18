0c7e6f2901bd383481797b99bdc46e6ba00872d1
juliaezzo <ezzojulia5@gmail.com>
2025-09-18 20:49:05 +0300

    add 1 & 2

---
+++ main.js
@@ -0,0 +1,19 @@
+// 1:Convert each sentence into words and save all words in a single array.
+// let sentences = ["I love JS", "JS is fun"];
+// let words = sentences.map(n => n.split(" "));
+// // console.log(words);
+// console.log(words.flat());
+
+
+
+// 2. Print each word with last letter + join words
+// let words = ["Hello", "World", "JS"];
+// words.forEach (words => {
+//     let lasstChar = words[words.length-1];
+//     console.log(words + " " + lasstChar);
+// })
+
+
+
+
+
