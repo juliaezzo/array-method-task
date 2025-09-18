f546268c15eed6a04375130ddcb31627c85eda6a
juliaezzo <ezzojulia5@gmail.com>
2025-09-18 22:15:51 +0300

    add4

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // 1:Convert each sentence into words and save all words in a single array.
+
 // let sentences = ["I love JS", "JS is fun"];
 // let words = sentences.map(n => n.split(" "));
 // // console.log(words);
@@ -7,6 +8,7 @@
 
 
 // 2. Print each word with last letter + join words
+
 // let words = ["Hello", "World", "JS"];
 // words.forEach (words => {
 //     let lasstChar = words[words.length-1];
@@ -18,7 +20,22 @@
 
 
 //  3. Reverse array and take first 4 elements
+
 // let nums = [10, 20, 30, 40, 50, 60];
 // console.log(nums.reverse());
 // let nums2 = nums.slice(0,4);
 // console.log(nums2);
+
+
+
+
+// 4:Remove "Banana" and replace it with two new words, Add a word at the end of the array, Remove the last 
+// element from the array.
+
+// let fruits = ["Apple", "Banana", "Orange", "Mango"];
+// fruits.splice(1,1,"Melon" , "peenapple");
+// console.log(fruits);
+// fruits.push("fru");
+// console.log(fruits);
+// fruits.pop("Mango")
+// console.log(fruits);
