10269fabc8d16f975b4819c67259b4f3c6f750e7
juliaezzo <ezzojulia5@gmail.com>
2025-09-19 13:24:13 +0300

    add7

--- main.js
+++ main.js
@@ -65,3 +65,49 @@
 // cities.sort();
 // console.log(cities);
 // console.log(cities.indexOf("Damascus"));
+
+
+
+
+
+
+// 7: 1. Split the text into words. 
+//    2. Remove very short words. 
+//    3. Take a specific number of first words. 
+//    4. Reverse the order of the words. 
+//    5. Add a word at the beginning and another at the end. 
+//    6. Check if a specific word exists. 
+//    7. Print each word on a separate line. 
+//    8. Join the words into a single string separated by -. 
+//    9. Sort the words alphabetically. 
+//    10. Print the index of a specific word after sorting.
+
+
+// let text = "JavaScript is fun but challenging when you want to master it quickly";
+// let text2 = text.split(" ")
+// console.log(text2);
+
+// let t = 3;
+// let filtered = text2.filter(word => {
+//     return word.length > t;
+// })
+//  console.log(filtered);
+
+// console.log(filtered.indexOf("challenging"));
+
+// console.log(filtered.reverse());
+
+// filtered.unshift("HTML");
+// console.log(filtered);
+
+// console.log(filtered.includes("CSS"));
+
+// filtered.forEach(word => {
+//     console.log(word);
+// });
+
+
+// console.log(filtered.join(","));
+
+// filtered.sort((a,b) => a.localeCompare (b));
+// console.log(filtered);
